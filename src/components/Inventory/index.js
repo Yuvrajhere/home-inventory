109f7b68a7480e079b7c7f06655b30b1c5256447
Yuvrajhere <yuvrajisbest13@gmail.com>
2020-10-03 23:53:24 +0530

    Added forms for inventory page

--- src/components/Inventory/index.js
+++ src/components/Inventory/index.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
+import {Link} from "react-router-dom";
 import "./Inventory.css";
+import NewProductFormImg from "../../assets/shopping-2.svg"
 
 const products = [
   {
@@ -31,88 +33,149 @@ const products = [
 const Inventory = () => {
   const [productsData, setProductsData] = useState(products);
 
+  const [newProductData, setNewProductData] = useState({
+    name: "",
+    quantityConsumption: "",
+    daysConsumption: ""
+  });
+
+  const handleInputChange = e => {
+    setNewProductData({
+      ...newProductData,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+  }
+
   return (
     <div className="Inventory">
-    <h1>Inventory</h1>
-     {(productsData.length > 0) ? (
-    <div>
-      <h2>Your Inventory</h2>
-      <table>
-        <tr>
-          <th>Product Name</th>
-          <th>Last Added Date</th>
-          <th>Last Added Quantity</th>
-          <th>Remaining Days</th>
-        </tr>
-
-        {productsData.map((product, index) => {
-          return (
+      <h1>Inventory</h1>
+      {productsData.length > 0 ? (
+        <div>
+          <h2>Your Inventory</h2>
+          <table>
             <tr>
-              <td>{product.name}</td>
-              <td>{product.lastAddedDate}</td>
-              <td>{product.lastAddedQuantity}</td>
-              <td>{product.daysConsumption}</td>
+              <th>Product Name</th>
+              <th>Last Added Date</th>
+              <th>Last Added Quantity</th>
+              <th>Remaining Days</th>
             </tr>
-          );
-        })}
-      </table>
-      <h2>Your Products</h2>
-      <table>
-        <tr>
-          <th>Product Name</th>
-          <th>Quantity consumed</th>
-          <th>Days to consume</th>
-        </tr>
 
-        {productsData.map((product, index) => {
-          return (
+            {productsData.map((product, index) => {
+              return (
+                <tr>
+                  <td>{product.name}</td>
+                  <td>{product.lastAddedDate}</td>
+                  <td>{product.lastAddedQuantity}</td>
+                  <td>{product.daysConsumption}</td>
+                </tr>
+              );
+            })}
+          </table>
+          <h2>Your Products</h2>
+          <table>
             <tr>
-              <td>{product.name}</td>
-              <td>{product.quantityConsumption}</td>
-              <td>{product.daysConsumption}</td>
+              <th>Product Name</th>
+              <th>Quantity consumed</th>
+              <th>Days to consume</th>
             </tr>
-          );
-        })}
-      </table>
-    </div>
-   ) : (
-     <h1>No products added yet</h1>
-   )}
-      <div>
-        <form>
-          <h2>Add a new Product</h2>
-          <label>
-            Product Name
-            <input type="text" name="productName" />
-          </label>
-          <label>
-            Quantity consumed
-            <input type="text" name="productName" />
-          </label>
-          <label>
-            Days to consume
-            <input type="text" name="productName" />
-          </label>
-          <button>Submit</button>
+
+            {productsData.map((product, index) => {
+              return (
+                <tr>
+                  <td>{product.name}</td>
+                  <td>{product.quantityConsumption}</td>
+                  <td>{product.daysConsumption}</td>
+                </tr>
+              );
+            })}
+          </table>
+        </div>
+      ) : (
+        <h1>No products added yet</h1>
+      )}
+      <div className="SignUp">
+        <form onSubmit={handleSubmit}>
+          <h1>Add new product</h1>
+          <label for="name">Product Name</label>
+          <br />
+          <input
+            id="name"
+            type="text"
+            name="name"
+            value={newProductData.name}
+            onChange={handleInputChange}
+            autoComplete="off"
+          />
+          <br />
+          <label for="quantityConsumption">quantityConsumption</label>
+          <br />
+          <input
+            id="quantityConsumption"
+            type="text"
+            name="quantityConsumption"
+            value={newProductData.quantityConsumption}
+            onChange={handleInputChange}
+            autoComplete="off"
+          />
+          <br />
+          <label for="daysConsumption">daysConsumption</label>
+          <br />
+          <input
+            id="daysConsumption"
+            type="text"
+            name="daysConsumption"
+            value={newProductData.daysConsumption}
+            onChange={handleInputChange}
+          />
+          <br />
+          <button className="btn">Submit</button>
         </form>
+        <img src={NewProductFormImg} />
       </div>
-      <div>
-        <form>
-          <h2>Update Inventory</h2>
-          <label>
-            Dropdown
-            <input type="text" name="productName" />
-          </label>
-          <label>
-            Quantity consumed
-            <input type="text" name="productName" />
-          </label>
-          <label>
-            Days to consume
-            <input type="text" name="productName" />
-          </label>
-          <button>Submit</button>
+      )}
+      <div className="SignUp">
+        <form onSubmit={handleSubmit}>
+          <h1>Update Inventory</h1>
+          <label for="name">Product Name</label>
+          <br />
+          <input
+            id="name"
+            type="text"
+            name="name"
+            value={newProductData.name}
+            onChange={handleInputChange}
+            autoComplete="off"
+          />
+          <br />
+          <label for="quantityConsumption">quantityConsumption</label>
+          <br />
+          <input
+            id="quantityConsumption"
+            type="text"
+            name="quantityConsumption"
+            value={newProductData.quantityConsumption}
+            onChange={handleInputChange}
+            autoComplete="off"
+          />
+          <br />
+          <label for="daysConsumption">daysConsumption</label>
+          <br />
+          <input
+            id="daysConsumption"
+            type="text"
+            name="daysConsumption"
+            value={newProductData.daysConsumption}
+            onChange={handleInputChange}
+          />
+          <br />
+          <button className="btn">Submit</button>
         </form>
+        <img src={NewProductFormImg} />
       </div>
     </div>
   );
